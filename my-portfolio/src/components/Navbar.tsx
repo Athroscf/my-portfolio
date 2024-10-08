e978909a964af0939e89efb75d9592b564c8cc2e
Christopher Fiallos <fialloschris1@gmail.com>
2024-10-08 17:32:12 -0600

    update: Navbar component.

--- my-portfolio/src/components/Navbar.tsx
+++ my-portfolio/src/components/Navbar.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import ModeToggle from "./mode-toggle";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "./ui/navigation-menu";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { GithubIcon, Menu } from "lucide-react";
+import { buttonVariants } from "./ui/button";
 
 interface RouteProps {
   href: string;
@@ -26,6 +30,8 @@ const routeList: RouteProps[] = [
 ];
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <header className="sticky border-b-[1px] top-0 w-full bg-white dark:border-b-slate-700 dark:bg-background">
       <NavigationMenu className="mx-auto">
@@ -37,6 +43,45 @@ const Navbar = () => {
           {/* mobile */}
           <span className="flex md:hidden">
             <ModeToggle />
+
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger className="px-2">
+                <Menu className="flex md:hidden h-5 w-5" onClick={() => setIsOpen(true)}>
+                  <span className="sr-only">Menu Icon</span>
+                </Menu>
+              </SheetTrigger>
+              <SheetContent side={"left"}>
+                <SheetHeader>
+                  <SheetHeader>
+                    <SheetTitle className="text-xl font-bold">Shadcn/React</SheetTitle>
+                  </SheetHeader>
+                </SheetHeader>
+                <nav className="flex flex-col justify-center items-center gap-2 mt-4">
+                  {routeList.map(({ href, label}: RouteProps) => (
+                    <a
+                      rel="noreferrer noopener"
+                      key={label}
+                      href={href}
+                      onClick={() => setIsOpen(false)}
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      {label}
+                    </a>
+                  ))}
+                  <a
+                    rel="noreferrer noopener"
+                    href="https://github.com/leoMirandaa/shadcn-landing-page.git"
+                    target="_blank"
+                    className={`w-[110px] border ${buttonVariants({
+                      variant: "secondary",
+                    })}`}
+                  >
+                    <GithubIcon className="mr-2 w-5 h-5" />
+                    Github
+                  </a>
+                </nav>
+              </SheetContent>
+            </Sheet>
           </span>
         </NavigationMenuList>
       </NavigationMenu>
